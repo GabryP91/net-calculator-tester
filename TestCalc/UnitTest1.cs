6839764c180bd41753ff9d4d4bee9ef05f3fb956
Gabriele <gabriele.palma.bool@gmail.com>
2024-05-09 15:26:43 +0200

    Aggiungere i file di progetto.

---
+++ TestCalc/UnitTest1.cs
@@ -0,0 +1,43 @@
+using net_calculator_tester;
+namespace TestCalc
+{
+    public class Tests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            float result = calculator.Add(5, 3);
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void TestSubtract()
+        {
+            float result = calculator.Subtract(5, 3);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        [TestCase(2,2)]
+        public void TestDivide( float num1, float num2)
+        {
+            float result = calculator.Divide(num1, num2);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void TestMultiply()
+        {
+            float result = calculator.Multiply(5, 3);
+            Assert.AreEqual(15, result);
+        }
+    }
+}
---
+++ net-calculator-tester/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 
+Implementare i metodi di una semplice calcolatrice e di scrivere una classe di test per verificare che funzionino come ci aspettiamo!
+Creare una classe chiamata Calculator avente i seguenti metodi pubblici :
+float Add(float num1, float num2)
+float Subtract(float num1, float num2)
+float Divide(float num1, float num2)
+float Multiply(float num1, float num2)
+Creare in seguito una classe di test che abbia 4 metodi, ognuno per verificare il corretto funzionamento di ogni metodo presente nella classe Calculator.
+
+*/
+
+namespace net_calculator_tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
